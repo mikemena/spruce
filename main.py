8961a5e49b665b5895104fb187be31cb2351ad54
tokyo <tokyolabs@protonmail.com>
2024-01-08 12:48:21 -0500

    wip

--- main.py
+++ main.py
@@ -8,20 +8,19 @@ from datetime import datetime
 def serial_number_finder():
     root_directory = "project_unzipped/My_Big_Directory"
     pattern = r"N[A-Za-z]{3}-\d{5}"
-    all_matches = []
+    results = []
 
     for dirpath, dirnames, filenames in os.walk(root_directory):
         for filename in filenames:
             if filename.endswith(".txt"):
                 file_path = os.path.join(dirpath, filename)
-                try:
-                    with open(file_path, "r", encoding="utf-8") as file:
-                        for line in file:
-                            matches = re.findall(pattern, line)
-                            all_matches.extend(matches)
-                except Exception as e:
-                    print(f"Error reading file {file_path}: {e}")
-    return all_matches
+                with open(file_path, "r", encoding="utf-8") as file:
+                    file_content = file.read()
+                    matches = re.findall(pattern, file_content)
+                    for match in matches:
+                        results.append((filename, match))
+
+    return results
 
 
 beginning = time.time()
@@ -29,13 +28,16 @@ serial_numbers = serial_number_finder()
 count = Counter(serial_numbers)
 total_count = sum(count.values())
 ending = time.time()
-# print(serial_numbers)
 
 today_date = datetime.now()
 formatted_date = today_date.strftime("%m/%d/%Y")
 
 print("-" * 30)
 print("Search date: ", formatted_date)
+print(f"{'FILE':<15} {'SERIAL NO.':<15}")
+print(f"{'----':<15} {'----------':<15}")
+for filename, serial_number in serial_numbers:
+    print(f"{filename:<15} {serial_number:<15}")
 print("Numbers found: ", total_count)
 print("Search duration: ", ending - beginning, "seconds")
 print("-" * 30)
