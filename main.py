b7cf4cdb280f55e1f181fab48b2734b692a771bf
tokyo <tokyolabs@protonmail.com>
2024-01-07 19:49:31 -0500

    wip

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import os
+import re
 
 root_directory = "project_unzipped/My_Big_Directory"
 
-search_word = "search_word"
+pattern = r"N[A-Za-z]{3}-\d{5}"
 
 for dirpath, dirnames, filenames in os.walk(root_directory):
     for filename in filenames:
         if filename.endswith(".txt"):  # checks if the file is a text file
             file_path = os.path.join(dirpath, filename)
             with open(file_path, "r", encoding="utf-8") as file:
-                for line in file:
-                    if search_word in line:
-                        print(f"'{search_word}' found in {file_path}")
-                        break  # assuming you only want to know if the word exists at least once in the file
+                matches = re.findall(pattern, test_string)
+                for match in matches:
+                    print(match)
